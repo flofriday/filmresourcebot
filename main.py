88bd5926197b60b2058dae1593e1bf291c77d781
flofriday <flohacksfriday@gmail.com>
2019-11-21 21:06:06 +0100

    Add suggestions

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
 import telegram_
 import photos
+import database
 
 # Load the config
 config = json.load(open("./config.json"))
 photos.__init__(config)
+database.__init__(config)
 
 # Start the telegram bot (for now it is only telegram but in the future it will
 # be a lot more services)
--- telegram_.py
+++ telegram_.py
@@ -1,9 +1,14 @@
 from telegram.ext import Updater, CommandHandler
+from telegram.utils import helpers
 import os
 import threading
 import photos
 from textwrap import dedent
+import database
+import html
 
+admin_user = None
+                    
 def send_answer(update, context, text):
     context.bot.send_message(chat_id=update.effective_chat.id, 
     text=text, parse_mode="Markdown")
@@ -21,20 +26,25 @@ def start(update, context):
 
 def ideas(update, context):
     send_answer(update, context, "Collecting the best images for you ...")
+
+    number = 5
+    if len(context.args) == 1: 
+        number = int(context.args[0])
+
     try:
-        picutes = photos.get_inspiration_photos()
+        picutes = photos.get_inspiration_photos(number)
     except Exception as e:
         print("Error loading the inspiration images:" + str(e))
         send_answer(update, context, 
         "Sorry, but there was an error when I tried to find your images" + str(e))
 
     for picture in picutes:
-        caption = f"[Source]({picture.source_url})"
+        caption = f"[{picture.name}]({picture.source_url})"
         try:
             context.bot.send_photo(chat_id=update.effective_chat.id,
             photo=picture.url, parse_mode="Markdown", caption=caption)
         except Exception as e:
-            print(f"Unable to send photo{pictue} to telegram: {str(e)}")
+            print(f"Unable to send photo{picture} to telegram: {str(e)}")
 
 def search(update, context):
     send_answer(update, context, 
@@ -42,19 +52,37 @@ def search(update, context):
     pass
 
 def resources(update, context):
-    message = """Reddit:
-    r/photogrm"""
+    message = open("Resources.md")
     context.bot.send_message(chat_id=update.effective_chat.id,
-    text=dedent(message))
+    text=message)
 
 def suggestion(update, context):
-    send_answer(update, context, 
-    "This command is currently not implemented, sorry")
+    user_message = " ".join(context.args).strip()
+
+    # Check if the suggestion is empty
+    if user_message == "":
+        message = """
+        You need to provide your suggestion with the message like:
+        `/suggestion Please add a discord integration`
+        """
+        send_answer(update, context, dedent(message))
+        return
+
+    # Add the suggestion to the database
+    try:
+        database.add_suggestion(update.effective_user.id, "telegram", user_message)
+    except Exception as e:
+        print(f"Exception: {str(e)}")
+        send_answer(update, context, 
+        "Sorry, there was a problem uploading your suggestion, maybe try again later.")
+
+    message = f"{update.effective_user.first_name}, thank you so much for your suggestion 😊"
+    send_answer(update, context, message)
 
 def help(update, context):
     message = """
     Here is a list of things I can do:
-    /ideas - Send you 5 new photos from other creators to inspire you
+    /ideas - Send you some new photos from other creators to inspire you
     /search - Find 5 photos for your serachquery
     /resource - Send a list of resources you might like
     /suggestion - Write a suggestion to my creator, how he can improve me
@@ -64,10 +92,60 @@ def help(update, context):
     send_answer(update, context, dedent(message))
 
 def about(update, context):
-    send_answer(update, context, 
-    "This command is currently not implemented, sorry")
+    message = """
+    Hi, I am [flofriday](https://github.com/flofriday) wrote this bot for a 
+    friend, who is a photographer. This bot is open source and on 
+    [github](https://github.com/flofriday/filmresourcebot).
+
+    If you have any suggestions, you can use /suggestion or create a issue on 
+    github. 
+    """ 
+    send_answer(update, context, dedent(message))
+
+# TODO: this should only be possible by the admin user 
+def admin_suggestions(update, context):
+    if str(update.effective_user.id) != admin_user:
+        print(update.effective_user.id)
+        # One could only know this exists from reading this line 
+        send_answer(update, context, "Nice try, scriptkiddi")
+        return
+
+    try:
+        suggestions = database.get_and_clear_suggestions()
+    except Exception as e:
+        print(f"Exception: {str(e)}")
+        send_answer(update, context, "Unable to load the suggestions")
+
+    message = f"{len(suggestions)} new Suggestions"
+    for suggestion in suggestions:
+        user_id = suggestion[0]
+        platform = suggestion[1]
+        text = helpers.escape_markdown(suggestion[2])
+        if (platform == "telegram"):
+            message += f"\n[Telegram User](tg://user?{user_id}); {text}" 
+        else:
+            message += f"\n{platform}; {user_id}; {text}" 
+        
+    send_answer(update, context, message)
+    
+def admin_help(update, context):
+    if str(update.effective_user.id) != admin_user:
+        print(update.effective_user.id)
+        # One could only know this exists from reading this line 
+        send_answer(update, context, "Nice try, scriptkiddi")
+        return
+
+    message = """
+    /adminhelp - This help
+    /adminsuggestions - Print all suggestions and delete them from the db
+    """ 
+    send_answer(update, context, dedent(message)) 
 
 def run(config):
+    # Set the admin
+    global admin_user 
+    admin_user = config["telegram_admin_user"]
+
     # Configure the telegram client
     token = config["telegram_token_dev"]
     if os.environ.get("PROD") != None:
@@ -85,6 +163,9 @@ def run(config):
     dispatcher.add_handler(CommandHandler("suggestion", suggestion))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("about", about))
+
+    dispatcher.add_handler(CommandHandler("adminhelp", admin_help))
+    dispatcher.add_handler(CommandHandler("adminsuggestions", admin_suggestions))
 
     # Start the bot
     updater.start_polling()
