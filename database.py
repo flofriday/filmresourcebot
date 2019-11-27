766b34a343c4e39abcf621af2bde8b5ab949aece
flofriday <flohacksfriday@gmail.com>
2019-11-27 18:31:17 +0100

    Add /statistics command

--- database.py
+++ database.py
@@ -11,13 +11,44 @@ def __init__(config):
     conn = sqlite3.connect(db_path)
     c = conn.cursor()
 
-    # Create all necesarry tables 
-    #c.execute('''CREATE TABLE IF NOT EXISTS users
-    #         (user_id text, platform text, interactions integer)''')
+    #Create all necesarry tables 
+    c.execute('''CREATE TABLE IF NOT EXISTS users
+             (user_id text, platform text, interactions integer)''')
     c.execute('''CREATE TABLE IF NOT EXISTS suggestions
              (user_id text, platform text, suggestion text)''')
     conn.commit()
     conn.close()
+
+def add_user(user_id, platform):
+    """Adds a new user to the database"""
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("INSERT INTO users VALUES (?, ?, 1)", (user_id, platform))
+    conn.commit()
+    conn.close() 
+
+def increase_interaction(user_id, platform):
+    """Increase the interaction counter by 1 from the user passed to this function"""
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("UPDATE users SET interactions = interactions + 1 WHERE user_id = ? AND platform = ?", (user_id, platform))
+    changed_rows = conn.total_changes
+    conn.commit()
+    conn.close()
+
+    if changed_rows == 0: 
+        add_user(user_id, platform)
+
+def get_statistic():
+    """Returns 2 values, the number of users and the total interactions"""
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    users = c.execute("SELECT COUNT(*) FROM users").fetchone()[0]
+    interactions = c.execute("SELECT SUM(interactions) FROM users").fetchone()[0]
+    conn.commit()
+    conn.close()  
+
+    return users, interactions
 
 def add_suggestion(user, platform, suggestion):
     """Adds a new suggestion to the database"""
--- telegram_.py
+++ telegram_.py
@@ -1,4 +1,4 @@
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram.utils import helpers
 import os
 import threading
@@ -11,9 +11,14 @@ admin_user = None
                     
 def send_answer(update, context, text):
     context.bot.send_message(chat_id=update.effective_chat.id, 
-    text=text, parse_mode="Markdown")
+    text=text, parse_mode="Markdown", disable_web_page_preview=True)
+
+def increase_interaction(update):
+    database.increase_interaction(update.effective_user.id, "telegram")
 
 def start(update, context):
+    increase_interaction(update)
+
     username = update.effective_user.first_name
     message = f"""
     Hi *{username}*,
@@ -25,6 +30,7 @@ def start(update, context):
     help(update, context)
 
 def ideas(update, context):
+    increase_interaction(update)
     send_answer(update, context, "Collecting the best images for you ...")
 
     number = 5
@@ -47,6 +53,8 @@ def ideas(update, context):
             print(f"Unable to send photo{picture} to telegram: {str(e)}")
 
 def search(update, context):
+    increase_interaction(update)
+
     # Exit if there is no search query
     if len(context.args) == 0:
         send_answer(update, context, 
@@ -63,6 +71,7 @@ def search(update, context):
         send_answer(update, context, 
         "Sorry, but there was an error when I tried to find your images: " + str(e))
 
+    # Send all pictures
     for picture in pictures:
         caption = f"[{picture.name}]({picture.source_url})"
         try:
@@ -70,13 +79,20 @@ def search(update, context):
             photo=picture.url, parse_mode="Markdown", caption=caption)
         except Exception as e:
             print(f"Unable to send photo{picture} to telegram: {str(e)}")
-
+    
+    # Send a link for more pictures
+    url = "https://unsplash.com/s/photos/" + query
+    send_answer(update, context, f"More \"{query}\" images on [Unsplash.com]({url})")
 
 def resources(update, context):
+    increase_interaction(update)
+
     message = open("./Resources.md").read()
     send_answer(update, context, message)
 
 def suggestion(update, context):
+    increase_interaction(update)
+
     user_message = " ".join(context.args).strip()
 
     # Check if the suggestion is empty
@@ -90,7 +106,7 @@ def suggestion(update, context):
 
     # Add the suggestion to the database
     try:
-        database.add_suggestion(update.effective_user.username, "telegram", user_message)
+        database.add_suggestion(update.effective_user.name, "telegram", user_message)
     except Exception as e:
         print(f"Exception: {str(e)}")
         send_answer(update, context, 
@@ -99,19 +115,58 @@ def suggestion(update, context):
     message = f"{update.effective_user.first_name}, thank you so much for your suggestion 😊"
     send_answer(update, context, message)
 
+def statistics(update, context):
+    increase_interaction(update)
+
+    users, interactions = database.get_statistic()
+    message = f"Users: {users}\nServed Interactions: {interactions}"
+    send_answer(update, context, message)
+
 def help(update, context):
+    increase_interaction(update)
     message = """
     Here is a list of things I can do:
     /ideas - Send you some new photos from other creators to inspire you
     /search - Find 5 photos for your serachquery
-    /resource - Send a list of resources you might like
+    /resources - Send a list of resources you might like
     /suggestion - Write a suggestion to my creator, how he can improve me
+    /statistics - Some statistics on how this bot is being used
+    /privacy - Information on how I handle privacy
     /about - Learn something about me and my creator
     /help - View this help
     """
     send_answer(update, context, dedent(message))
 
+def privacy(update, context):
+    increase_interaction(update)
+    message = """
+    *--- Privacy ---*
+    Hi, I am [flofriday](https://github.com/flofriday), the creator of this bot.
+    Unfortunately, this bot saves some userdata. However, I promise that I will 
+    *never sell* this *data* to anybody. Moreover, I promise you that I will never
+    use any userdata to enrich myself in anyway.
+
+    *-- So why do you even collect any data? --*
+    First, some functionality is not possible without any userdata. For example
+    the email-less suggestion feature. Also I want to know whats going on my bot
+    therefore I created the /statistics command. I promise that every analytics 
+    tool for this bot will be available to all users, so everybody knows as much
+    as I do.
+
+    *-- What exactly do you collect) --*
+    Since the first time u used the bot I saved your telegram id with a counter 
+    of how many interactions you had with the bot. I do not save what you wrote 
+    the bot, what you received from the bot nor when you wrote the bot. Also I 
+    promise that I will never use those ids to contact you personally (I am not
+    even sure if that would be possible). 
+    In the case you write a suggestion, I will save your telegram username along
+    the suggestion text. With that I can (and in rare cases might) contact you 
+    to discuss your suggestion. 
+    """ 
+    send_answer(update, context, dedent(message)) 
+
 def about(update, context):
+    increase_interaction(update)
     message = """
     Hi, I am [flofriday](https://github.com/flofriday) and wrote this bot for a 
     friend, who is a photographer. This bot is being developed in python and is 
@@ -123,6 +178,7 @@ def about(update, context):
     send_answer(update, context, dedent(message))
 
 def admin_suggestions(update, context):
+    increase_interaction(update)
     # Stop unprviledged users
     if str(update.effective_user.id) != admin_user:
         print(update.effective_user.id)
@@ -149,14 +205,12 @@ def admin_suggestions(update, context):
         user_name = suggestion[0]
         platform = suggestion[1]
         text = helpers.escape_markdown(suggestion[2])
-        if (platform == "telegram"):
-            message += f"\n**{n+1}:** {platform} | @{user_name} | {text}" 
-        else:
-            message += f"\n**{n+1}:** {platform} | {user_name} | {text}" 
+        message += f"\n*{n+1}:* {platform} | {user_name} | {text}" 
         
     send_answer(update, context, message)
     
 def admin_help(update, context):
+    increase_interaction(update)
     if str(update.effective_user.id) != admin_user:
         print(update.effective_user.id)
         # One could only know this exists from reading this line 
@@ -168,6 +222,9 @@ def admin_help(update, context):
     /adminsuggestions - Print all suggestions and delete them from the db
     """ 
     send_answer(update, context, dedent(message)) 
+
+def unknown(update, context):
+    send_answer(update, context, "Sorry I didn't understand that. Try /help to see what I can understand.") 
 
 def run(config):
     # Set the admin
@@ -189,12 +246,17 @@ def run(config):
     dispatcher.add_handler(CommandHandler("search", search))
     dispatcher.add_handler(CommandHandler("resources", resources))
     dispatcher.add_handler(CommandHandler("suggestion", suggestion))
+    dispatcher.add_handler(CommandHandler("statistics", statistics))
+    dispatcher.add_handler(CommandHandler("privacy", privacy))
     dispatcher.add_handler(CommandHandler("help", help))
     dispatcher.add_handler(CommandHandler("about", about))
 
     dispatcher.add_handler(CommandHandler("admin", admin_help))
     dispatcher.add_handler(CommandHandler("adminhelp", admin_help))
     dispatcher.add_handler(CommandHandler("adminsuggestions", admin_suggestions))
+
+    dispatcher.add_handler(MessageHandler(Filters.command, unknown))
+    dispatcher.add_handler(MessageHandler(Filters.text, unknown))
 
     # Start the bot
     updater.start_polling()
